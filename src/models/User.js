7586c2a5c2e2c480ef68da6f81bd6d3090b64589
salih bozkaya <salihbozkaya@salihs-MacBook-Pro.local>
2022-08-09 09:36:50 +0300

    feat(model): implemented user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,28 +1,44 @@
-const { DataTypes } = require("sequelize/types");
+require("../utils/envLoader");
+const { DataTypes } = require("sequelize");
+const argon2 = require("argon2");
 
-const User = sequelize.define(
-    "User",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
+module.exports = sequelize => {
+    const User = sequelize.define(
+        "user",
+        {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            email: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                unique: true,
+                validate: {
+                    isEmail: true,
+                },
+            },
+            password: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                validate: {
+                    is: /^.{3,}$/,
+                },
+            },
         },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    },
-    {}
-);
+        {
+            tableName: "users",
+            hooks: {
+                beforeCreate: async (record, options) => {
+                    record.dataValues.password = await argon2.hash(
+                        record.dataValues.password
+                    );
+                },
+            },
+        }
+    );
 
-
-module.exports = User;
+    return User;
+};
